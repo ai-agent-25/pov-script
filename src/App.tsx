88bdc19f9830b8798a720b6fdec0d42cbd0e429f
Priya Aswani <priya.aswani@toptal.com>
2023-10-29 20:46:41 +0400

    Wrap App in ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { theme } from "./utils";
+
 export const App = () => {
-  return <p>Welcome to ai-agent powered by Protorisk</p>;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <p>Welcome to ai-agent powered by Protorisk</p>
+    </ThemeProvider>
+  );
 };
