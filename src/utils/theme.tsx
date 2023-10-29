fb731b1d5ea697590dad9fd29974a7a22fc9598b
Priya Aswani <priya.aswani@toptal.com>
2023-10-29 20:46:08 +0400

    Create theme in utils

---
+++ src/utils/theme.tsx
@@ -0,0 +1,9 @@
+import { deepPurple } from "@mui/material/colors";
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: { main: deepPurple["A700"] },
+  },
+});
