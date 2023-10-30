8dc51ebbe81ce7cb42c8c4c9464f0d68295cae25
Priya Aswani <priya.aswani@toptal.com>
2023-10-30 12:44:35 +0400

    Render TabBar in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import { theme } from "./utils";
+import { TabBar } from "./components";
 
 export const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <p>Welcome to ai-agent powered by Protorisk</p>
+      <TabBar />
     </ThemeProvider>
   );
 };
