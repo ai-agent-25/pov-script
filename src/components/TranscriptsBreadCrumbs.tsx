6a71d810198316db3bae76655af7abd0735e3378
Priya Aswani <priya.aswani@toptal.com>
2023-10-31 22:59:54 +0400

    Dynamically receive content to render for breadcrumbs

--- src/components/TranscriptsBreadCrumbs.tsx
+++ src/components/TranscriptsBreadCrumbs.tsx
@@ -1,15 +1,29 @@
 import Breadcrumbs from "@mui/material/Breadcrumbs";
 import Chip from "@mui/material/Chip";
+import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
 import Stack from "@mui/material/Stack";
+import { TranscriptsBreadCrumbsPropsType } from "../types/types";
 
-export const TranscriptsBreadCrumbs = () => {
-  const breadcrumbs = [
-    <Link underline="hover" color="inherit" href="/" onClick={() => {}}>
-      Transcripts
-    </Link>,
-    <Chip label="Speaker" color="primary" />,
-  ];
+export const TranscriptsBreadCrumbs = ({
+  links,
+  handleBreadCrumbClick,
+}: TranscriptsBreadCrumbsPropsType) => {
+  const breadcrumbs =
+    links.length === 1
+      ? [<Typography color="text.primary">{links[0]}</Typography>]
+      : [
+          <Link
+            underline="hover"
+            color="inherit"
+            href="/"
+            onClick={handleBreadCrumbClick}
+          >
+            {links[0]}
+          </Link>,
+          <Chip label={links[1]} color="primary" />,
+        ];
+
   return (
     <Stack spacing={2}>
       <Breadcrumbs separator="›" aria-label="breadcrumb">
