152bc2a26519f75bd5a44a963963b120868e80cb
Priya Aswani <priya.aswani@toptal.com>
2023-10-31 22:40:39 +0400

    Create placeholder component for Transcripts window

---
+++ src/components/Transcripts.tsx
@@ -0,0 +1,10 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+export const Transcripts = () => {
+  return (
+    <Box>
+      <Typography>Transcripts</Typography>
+    </Box>
+  );
+};
