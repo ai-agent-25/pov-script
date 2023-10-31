af9da423ce8a600112ace071f2cf8b4fbd782ae1
Priya Aswani <priya.aswani@toptal.com>
2023-10-31 22:21:53 +0400

    Create placeholder component for UserChat window

---
+++ src/components/UserChat.tsx
@@ -0,0 +1,10 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+export const UserChat = () => {
+  return (
+    <Box>
+      <Typography>@User Chat</Typography>
+    </Box>
+  );
+};
