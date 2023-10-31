c9659a56538b16842d4b5a4e3e8b3eacc1b84ef7
Priya Aswani <priya.aswani@toptal.com>
2023-10-31 22:57:38 +0400

    Create a list of speakers in Transcripts

--- src/components/Transcripts.tsx
+++ src/components/Transcripts.tsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 
 export const Transcripts = () => {
+  const [speakers, setSpeakers] = useState<string[]>([
+    "Writer",
+    "Editor",
+    "IdeaGenerator",
+    "Researcher",
+    "PlotAdvisor",
+    "WorldBuilder",
+    "Formatter",
+    "TimelineManager",
+    "Critic",
+    "@User",
+  ]);
+
   return (
     <Box>
       <Typography>Transcripts</Typography>
