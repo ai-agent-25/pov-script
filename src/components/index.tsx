f3e4203e519fc612eedea947b6ec87be37aa7b27
Priya Aswani <priya.aswani@toptal.com>
2023-11-01 20:57:12 +0400

    Export Transcripts via components' index.tsx

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,3 +2,4 @@ export { ChatHistory } from "./ChatHistory";
 export { Script } from "./Script";
 export { StepThrough } from "./StepThrough";
 export { TabBar } from "./TabBar";
+export { Transcripts } from "./Transcripts";
