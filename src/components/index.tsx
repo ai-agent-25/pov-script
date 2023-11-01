a92cffae1278d053dceca9baff7fa396e5b17c04
Priya Aswani <priya.aswani@toptal.com>
2023-11-01 20:58:54 +0400

    Export FinUserChat from components' index.tsx

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,5 @@
 export { ChatHistory } from "./ChatHistory";
+export { FinUserChat } from "./FinUserChat";
 export { Script } from "./Script";
 export { StepThrough } from "./StepThrough";
 export { TabBar } from "./TabBar";
