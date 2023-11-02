afc7b9167d98def34b686ffc128c5f84ef16300f
Priya Aswani <priya.aswani@toptal.com>
2023-11-02 17:52:11 +0400

    Export SpeakerTranscript via components' index.tsx

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,7 @@
 export { ChatHistory } from "./ChatHistory";
 export { FinUserChat } from "./FinUserChat";
 export { Script } from "./Script";
+export { SpeakerTranscript } from "./SpeakerTranscript";
 export { StepThrough } from "./StepThrough";
 export { TabBar } from "./TabBar";
 export { Transcripts } from "./Transcripts";
