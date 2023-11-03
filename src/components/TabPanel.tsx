e833296fdeb2b55097d99aed5e36c0c9367fb609
Priya Aswani <priya.aswani@toptal.com>
2023-11-03 20:59:21 +0400

    Create TabPanel component

---
+++ src/components/TabPanel.tsx
@@ -0,0 +1,17 @@
+import Box from "@mui/material/Box";
+import { TabPanelPropsType } from "../types/types";
+
+export const TabPanel = (props: TabPanelPropsType) => {
+  const { children, value, index, ...other } = props;
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+};
