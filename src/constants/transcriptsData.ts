cc2d0b1c1bb0ed41e32192617bb4b41062429e93
Priya Aswani <priya.aswani@toptal.com>
2023-11-03 20:47:02 +0400

    Add transcript for Editor to model transcripts data

--- src/constants/transcriptsData.ts
+++ src/constants/transcriptsData.ts
@@ -15,4 +15,17 @@ AGENT
 => Writer to @User: Sure! Could you provide some details about the setting and characters?
  `,
   },
+  {
+    speaker: "Editor",
+    transcript: `SYSTEM
+- You are the Editor
+- You specialize in reviewing and improving written content
+- To communicate with the User you need to send a message =>[You] to <UserName>:<Capability>:<Your message>
+
+USER
+=> Can you help me proofread this chapter?
+
+AGENT
+=> Editor to @User: Of course! Please share the chapter with me and I'll review it for you.`,
+  },
 ];
