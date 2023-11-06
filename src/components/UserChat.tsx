7fb86502e15e3ad5c88256b338ed1230d5f566be
Priya Aswani <priya.aswani@toptal.com>
2023-11-06 16:03:20 +0400

    Add a chat bar to UserChat

--- src/components/UserChat.tsx
+++ src/components/UserChat.tsx
@@ -1,10 +1,22 @@
+import { Paper } from "@mui/material";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
 
 export const UserChat = () => {
   return (
-    <Box>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        height: "100%",
+      }}
+    >
       <Typography>@User Chat</Typography>
+      <Paper sx={{ m: -3, p: 3 }}>
+        <TextField fullWidth id="message" variant="filled" size="small" />
+      </Paper>
     </Box>
   );
 };
