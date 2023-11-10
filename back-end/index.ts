78eef737a88fb09aabbd3ba821320358f29b5800
Priya Aswani <priya.aswani@toptal.com>
2023-11-10 19:32:24 +0400

    Use cors as middleware in express app

--- back-end/index.ts
+++ back-end/index.ts
@@ -1,11 +1,13 @@
 import express, { Express } from "express";
 import bodyParser from "body-parser";
 import morgan from "morgan";
+import cors from "cors";
 
 const port: number = 8080;
 const app: Express = express();
 
 app.use(morgan("dev"));
+app.use(cors());
 app.use(bodyParser.json());
 
 app.listen(port, () => {
