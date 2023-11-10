eb0ef3fef4253a09d8cd894c706100d37ae5881d
Priya Aswani <priya.aswani@toptal.com>
2023-11-10 19:31:11 +0400

    Use morgan as middleware

--- back-end/index.ts
+++ back-end/index.ts
@@ -1,7 +1,10 @@
 import express, { Express } from "express";
+import morgan from "morgan";
 
 const port: number = 8080;
 const app: Express = express();
+
+app.use(morgan("dev"));
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
