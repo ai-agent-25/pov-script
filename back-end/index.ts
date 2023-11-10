fdb6d89a662d234e71f963f50baeaa60dd5be486
Priya Aswani <priya.aswani@toptal.com>
2023-11-10 19:31:57 +0400

    Use bodyParser as middleware in express app

--- back-end/index.ts
+++ back-end/index.ts
@@ -1,10 +1,12 @@
 import express, { Express } from "express";
+import bodyParser from "body-parser";
 import morgan from "morgan";
 
 const port: number = 8080;
 const app: Express = express();
 
 app.use(morgan("dev"));
+app.use(bodyParser.json());
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
