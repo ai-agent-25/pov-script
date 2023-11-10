ad09c5c4ffd394765e792f2d94b959c1a16eeb94
Priya Aswani <priya.aswani@toptal.com>
2023-11-10 19:35:10 +0400

    Create a post endpoint in router to make OpenAI call

--- back-end/routes/Api.ts
+++ back-end/routes/Api.ts
@@ -1,3 +1,23 @@
-import express, { Router } from "express";
+import express, { Router, Request, Response } from "express";
+import { openai } from "../utils/globals";
 
 export const router: Router = express.Router();
+
+router.post("/", async (req: Request, res: Response) => {
+  const messages = req.body;
+  openai.chat.completions
+    .create({
+      messages,
+      model: "gpt-4",
+    })
+    .then((data) => {
+      return res
+        .status(200)
+        .json({ confirmation: "success", data: data.choices[0].message });
+    })
+    .catch((error) => {
+      return res
+        .status(400)
+        .json({ confirmation: "fail", error: error.message });
+    });
+});
