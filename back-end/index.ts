467529974c9a3704f827d607491a413d9909eec0
Priya Aswani <priya.aswani@toptal.com>
2023-11-10 19:36:43 +0400

    Use router as middleware in express app

--- back-end/index.ts
+++ back-end/index.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 import bodyParser from "body-parser";
 import morgan from "morgan";
 import cors from "cors";
+import { router } from "./routes/Api";
 
 const port: number = 8080;
 const app: Express = express();
@@ -9,6 +10,7 @@ const app: Express = express();
 app.use(morgan("dev"));
 app.use(cors());
 app.use(bodyParser.json());
+app.use("/", router);
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
