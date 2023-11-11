d5887c44169789a825ebc31ec18980eeb1ff9211
Priya Aswani <priya.aswani@toptal.com>
2023-11-11 16:41:13 +0400

    Create createMessage in front-end services

---
+++ src/services/Api.ts
@@ -0,0 +1,18 @@
+import { MessagesType } from "../types/types";
+const BASE_URL = "http://localhost:8080";
+
+export const createMessage = async (messages: MessagesType[]) => {
+  return fetch(`${BASE_URL}/`, {
+    mode: "cors",
+    method: "post",
+    cache: "no-cache",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(messages),
+  })
+    .then((response) => response.json())
+    .then((data) => data.data)
+    .catch((error) => error);
+};
