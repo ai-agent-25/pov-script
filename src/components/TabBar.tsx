4b46d649c90dfc3ec7712e719ce0339f7598e3eb
Priya Aswani <priya.aswani@toptal.com>
2023-11-12 22:50:12 +0400

    Create and track messages, setMessages state in TabBar

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -13,7 +13,7 @@ import {
   Transcripts,
   UserChat,
 } from ".";
-import { TabType } from "../types/types";
+import { MessagesType, TabType } from "../types/types";
 import { scriptData } from "../constants";
 import { cloneDeep, filter, split } from "lodash";
 
@@ -46,8 +46,18 @@ export const TabBar = () => {
   const [value, setValue] = useState(0);
   const [script, setScript] = useState<string>(scriptData);
   const [roles, setRoles] = useState<string[] | null>(null);
+  const [messages, setMessages] = useState<MessagesType[]>([
+    {
+      role: "system",
+      content:
+        "SYSTEM\n- You are Manager\n- To communicate with the User you need to send a message =><Your role> to @<UserName>:<Capability>: <Your message>\n- To communicate with Speakers you need to send a message =><Your role> to <Speaker>: <Your message>\n- Please note that you must use @ only when communicating with actual users, if you are referring to your own role or that of another automated agent, just mention the role without @\n- The system has some keywords. ANything in you see in all caps are keywords. Do not change the way you express them in your responses.\nUSER:\n=>@User to Manager: Hi\n- Respond appropriately to the user and ask the user if they want to create an investor report\n- If the user asked you to create the investor report first confirm your understanding with the user and if they confirm respond with ->DIALOG('CreateReport')\n- If the user also asked you to send out the investor report to the investors, make a note of that\n- If you need to abandon the process, tell the user you are abandoning and call ->ENDAGENT",
+    },
+  ]);
   const [tabs, setTabs] = useState<TabType[]>([
-    { tab: "@User Chat", component: <UserChat /> },
+    {
+      tab: "@User Chat",
+      component: <UserChat />,
+    },
     { tab: "@FinUser Chat", component: <FinUserChat /> },
     { tab: "Chat History", component: <ChatHistory /> },
     { tab: "Transcripts", component: <Transcripts roles={roles} /> },
@@ -61,6 +71,7 @@ export const TabBar = () => {
   useEffect(() => {
     let splitOnRole = script.split("ROLE: ");
     splitOnRole.splice(0, 1);
+    console.log(splitOnRole);
 
     let rolesFromScript = new Map();
     splitOnRole.map((item) => {
@@ -68,13 +79,16 @@ export const TabBar = () => {
       role = role.split(" ")[0];
       let splitOnAction = item.split(/-(?!>)/);
       splitOnAction.splice(0, 1);
+      console.log(splitOnAction);
       splitOnAction = splitOnAction.map((actionItem) =>
         actionItem.replace(/^\s*|\s*$/g, "").replace(/\n[\s\S]*$/, "")
       );
+      // console.log(role, splitOnAction);
       if (!rolesFromScript.has(role)) {
         rolesFromScript.set(role, splitOnAction);
       }
     });
+    console.log(rolesFromScript);
     let newRoles: string[] = Array.from(rolesFromScript.keys()) as string[];
     setRoles(newRoles);
   }, [script]);
