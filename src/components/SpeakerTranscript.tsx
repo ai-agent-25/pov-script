3ed04cc1956bac6d05a23f4c60e2f51586382cf9
Priya Aswani <priya.aswani@toptal.com>
2023-11-13 08:48:06 +0400

    Display selected script in SpeakerTranscript

--- src/components/SpeakerTranscript.tsx
+++ src/components/SpeakerTranscript.tsx
@@ -1,30 +1,14 @@
-import { useCallback, useState } from "react";
 import { aura } from "@uiw/codemirror-theme-aura";
 import CodeMirror from "@uiw/react-codemirror";
+import { SpeakerTranscriptPropsType } from "../types/types";
 
-export const SpeakerTranscript = () => {
-  const [script, setScript] = useState(`SYSTEM
-- You are WriterAgent
-- You excel at generating written content
-- To communicate with the User you need to send a message =>[You] to <UserName>:<Capability>:<Your message>
-
-USER
-=> I need help with writing the opening scene of my story.
-
-AGENT
-=> Writer to @User: Sure! Could you provide some details about the setting and characters?
- `);
-  const onChange = useCallback((val: string) => {
-    setScript(val);
-  }, []);
-
+export const SpeakerTranscript = ({ script }: SpeakerTranscriptPropsType) => {
   return (
     <CodeMirror
       editable={false}
       value={script}
       maxHeight="65vh"
       placeholder={"Your script here."}
-      onChange={onChange}
       theme={aura}
     />
   );
