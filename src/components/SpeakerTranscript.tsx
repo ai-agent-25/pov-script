6dbd5ba9f04d1bc3c09443ad3f4a45acb3d7aaef
Priya Aswani <priya.aswani@toptal.com>
2023-11-13 15:23:02 +0400

    Prevent horizontal scroll by adding line wrap in SpeakerTranscript

--- src/components/SpeakerTranscript.tsx
+++ src/components/SpeakerTranscript.tsx
@@ -1,3 +1,4 @@
+import { EditorView } from "@codemirror/view";
 import { aura } from "@uiw/codemirror-theme-aura";
 import CodeMirror from "@uiw/react-codemirror";
 import { SpeakerTranscriptPropsType } from "../types/types";
@@ -5,11 +6,17 @@ import { SpeakerTranscriptPropsType } from "../types/types";
 export const SpeakerTranscript = ({ script }: SpeakerTranscriptPropsType) => {
   return (
     <CodeMirror
+      extensions={[EditorView.lineWrapping]}
       editable={false}
       value={script}
       maxHeight="65vh"
       placeholder={"Your script here."}
       theme={aura}
+      style={{
+        whiteSpace: "pre-wrap",
+        wordBreak: "normal",
+        wordWrap: "break-word",
+      }}
     />
   );
 };
